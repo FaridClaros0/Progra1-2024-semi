b382bf8978657ba8e6902b2d4976e7beaa8f2712
FaridClaros <faridclaros15@gmail.com>
2024-07-30 08:02:40 -0600

    presione s para seguir ejecutando

--- PrimerProyecto/PrimerProyecto/Program.cs
+++ PrimerProyecto/PrimerProyecto/Program.cs
@@ -12,35 +12,45 @@ namespace PrimerProyecto
         {
             //estructuras de control
             // 1. if. ejercicio de mayoria de edad
-            Console.Write("Edad:");
-            int edad = int.Parse(Console.ReadLine());
-
-            if (edad <0) {
-                Console.WriteLine("Edad incorrecta");
-            }
-            else if (edad <=2) {
-                Console.WriteLine("Eres un bebe");
-            }
-            else if (edad <12) 
+            string continuar = "s";
+            while (continuar  == "s")
             {
-                 Console.WriteLine("Eres un niño");
-            } else if (edad <18)
-            { 
-                Console.WriteLine("Eres un adolescente");
-            }
-            else if (edad <=65)
-            {
-                Console.WriteLine("Bienvenido al mundo de las responsabilidades");  
-            } else if (edad <=80)
-            {
-                Console.WriteLine("Eres un adulto mayror");
-            } else {
-                Console.WriteLine("Larga vida");
-            }
 
-            //Pausa.
-            Console.ReadLine();
+                Console.Write("Edad:");
+                int edad = int.Parse(Console.ReadLine());
 
+                if (edad < 0)
+                {
+                    Console.WriteLine("Edad incorrecta");
+                }
+                else if (edad <= 2)
+                {
+                    Console.WriteLine("Eres un bebe");
+                }
+                else if (edad < 12)
+                {
+                    Console.WriteLine("Eres un niño");
+                }
+                else if (edad < 18)
+                {
+                    Console.WriteLine("Eres un adolescente");
+                }
+                else if (edad <= 65)
+                {
+                    Console.WriteLine("Bienvenido al mundo de las responsabilidades");
+                }
+                else if (edad <= 80)
+                {
+                    Console.WriteLine("Eres un adulto mayror");
+                }
+                else
+                {
+                    Console.WriteLine("Larga vida");
+                }
+                Console.WriteLine("Desea continuar presione s, sino cualquier tecla. \n");
+                continuar = Console.ReadLine();
+                
+            }
         }
     }
 }
