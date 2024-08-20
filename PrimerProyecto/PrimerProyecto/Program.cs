2fcb1e84e513cf76dc03c3d5b70f96c7cfa5877b
FaridClaros <faridclaros15@gmail.com>
2024-08-20 16:08:39 -0600

    Calculo de la media armonica

--- PrimerProyecto/PrimerProyecto/Program.cs
+++ PrimerProyecto/PrimerProyecto/Program.cs
@@ -8,17 +8,19 @@ namespace PrimerProyecto
 {
     internal class Program
     {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             //  ejercicio de sacar la media aritmetca y la desviacion tipica de una serie de numeros    
             Console.Write("Ingrese los numeros separados por comas: ");
             string serie = Console.ReadLine();
             string[] numeros = serie.Split(',');
 
-            Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1}", media(numeros), tipica(numeros));
+            Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1}, la media armonica es: {2}",
+                 media(numeros), tipica(numeros), armonica(numeros));
 
             Console.ReadLine();
         }
-            static double media(string[] serie)
+        static double media(string[] serie)
         {
             double media = 0;
             foreach (string num in serie)
@@ -33,11 +35,21 @@ namespace PrimerProyecto
                     m = media(serie);
             foreach (string num in serie)
             {
-                tipica += Math.Pow(int.Parse (num) - m, 2);
+                tipica += Math.Pow(int.Parse(num) - m, 2);
             }
             tipica = Math.Sqrt(tipica / serie.Length);
             return Math.Round(tipica, 2);
         }
+        static double armonica(string[] serie)
+        {
+            double armonica = 0;
+            foreach (string num in serie)
+            {
+                armonica += (1.0 / double.Parse(num));
+            }
+            armonica = serie.Length / armonica;
+            return Math.Round(armonica, 2);
+        }
     }
-}
+       }
  
